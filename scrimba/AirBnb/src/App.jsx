a3202c24e49299f26114f21307e32130892c05b9
Kwame Cody <kwamecody@gmail.com>
2024-03-01 18:08:07 +0000

    final styles

--- scrimba/AirBnb/src/App.jsx
+++ scrimba/AirBnb/src/App.jsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import Nav from './components/Nav'
 import Hero from './components/Hero'
+import Card from './components/Card'
+import data from './data'
 
 
 function App() {
-
+   const cards = data.map(card => {
+      return(
+         <Card
+           key = {card.id}
+           {...card}
+         />
+      )
+   })
   return (
      <div>
         <Nav/>
         <Hero/>
+        <section className='cards--list'>
+            {cards}
+        </section>
      </div>
   )
 }
