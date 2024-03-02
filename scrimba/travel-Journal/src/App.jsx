7c928aac39fcfa186989c5f9bbb4dde26c8e745d
Kwame Cody <kwamecody@gmail.com>
2024-03-02 06:45:15 +0000

    final styles

--- scrimba/travel-Journal/src/App.jsx
+++ scrimba/travel-Journal/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 import Nav from './components/Nav'
+import Card from './components/Card'
 import travelData from './data'
 
 function App() {
   
+   const data = travelData.map(item => {
+      return(
+         <Card
+           key = {item.id}
+           {...item}
+         />
+      )
+   })
 
   return (
      <div className='container'>
         <Nav/>
+        <section className='cards'>
+            {data}
+        </section>
      </div>
   )
 }
--- scrimba/travel-Journal/src/components/Nav.jsx
+++ scrimba/travel-Journal/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Nav() {
   return (
     <nav className="nav">
-        <div className="nav--items">
+        <div className="nav--items flex">
              <img src="../src/assets/globe.png" alt="Globe icon" />
              <h1 className="nav--title">My Travel Journal</h1>
         </div>
