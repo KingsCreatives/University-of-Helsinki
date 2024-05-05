324998d636d422a309e99f48e85ddb84be66fcfa
kwamecody <kwamecody@gmail.com>
2024-05-05 08:23:21 +0000

    exercise 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -16,19 +16,32 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
-     <Part part={part1} exercise={exercises1}/>
-     <Part part={part2} exercise={exercises2}/>
-     <Part part={part3} exercise={exercises3}/>
-     <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <h1>{course}</h1>
+      <p>
+        {part1.name} {part1.name}
+      </p>
+      <p>
+        {part2.name} {part2.name}
+      </p>
+      <p>
+        {part3.name} {part3.name}
+      </p>
+      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
     </div>
   );
 };
