d488264d73ea4d08284816f3d32e36ecca6e7da3
kwamecody <kwamecody@gmail.com>
2024-05-11 17:22:20 +0000

    merge conflicts

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,27 @@
+const Header = ({title}) => <h1>{title}</h1>
+
+const Content = ({part, exercise}) => <p>{part} {exercise}</p>
+
+const Total = ({ first, second, third }) => <p>Number of exercises{first + second + third}</p>;
+
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  return (
+    <div>
+      <Header title={course}/>
+      <Content part={part1} exercise={exercises1}/>
+      <Content part={part2} exercise={exercises2}/>
+      <Content part={part3} exercise={exercises3}/>
+      <Total first={exercises1} second={exercises2} third={exercises3}/>
+    </div>
+  );
+};
+
+export default App;
