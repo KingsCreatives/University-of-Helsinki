afa90f0163af22a386600aa7216c7310d5ab8628
kwame cody <kwamecody@gmail.com>
2024-08-10 16:53:32 +0100

    exercise3.2: return time and phonebook count in info route

--- part3/index.js
+++ part3/index.js
@@ -10,6 +10,22 @@ app.get("/api/persons", (req, res) => {
   return res.status(200).json(contact);
 });
 
+app.get("/info", (req, res) => {
+  const text = `Phonebook has info for ${contact.length} person`
+ const date = new Date().toLocaleString('en-US', { 
+    weekday: 'short', 
+    year: 'numeric', 
+    month: 'short', 
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    timeZoneName: 'long'
+  });
+  const response = `${text}\n${date}`;
+   return res.send(response)
+})
+
 app.listen(PORT, () => {
   console.log(`app is running on PORT:${PORT}`);
 });
