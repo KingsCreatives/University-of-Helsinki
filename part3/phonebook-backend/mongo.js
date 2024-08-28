95116505a59e58f82d48afdd69e15837f5d27d40
kwame cody <kwamecody@gmail.com>
2024-08-28 18:02:22 +0000

    update: seperate concerns for readability

--- part3/phonebook-backend/mongo.js
+++ part3/phonebook-backend/mongo.js
@@ -1,44 +1,66 @@
 const mongoose = require("mongoose");
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
+  console.log("Please provide the password as an argument.");
   process.exit(1);
 }
 
-const password = process.argv[2];
-const contactName = process.argv[3];
-const contactNumber = process.argv[4];
+const [password, contactName, contactNumber] = process.argv.slice(2);
 
 const url = `mongodb+srv://kings:${password}@fullstack.bxq3y.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=fullstack`;
 
 mongoose.set("strictQuery", false);
 
-mongoose.connect(url);
+ const phonebookSchema = new mongoose.Schema({
+   name: String,
+   telephone: String,
+ });
 
-const phonebookSchema = new mongoose.Schema({
-  name: String,
-  telephone: String,
-});
+ const Contact = mongoose.model("Contact", phonebookSchema);
 
-const Contact = mongoose.model("Contact", phonebookSchema);
-
-const phonebook = new Contact({
-  name: contactName,
-  telephone: contactNumber,
-});
-
-if (contactName && contactNumber) {
-  phonebook.save().then((result) => {
-    mongoose.connection.close();
-    console.log(`added ${contactName} number ${contactNumber} to phonebook`);
-  });
-} else {
-  Contact.find({}).then((persons) => {
-    let message = "phonebook:";
-    persons.forEach((person) => {
-      message += `\n ${person.name} ${person.telephone}`;
-    });
-    console.log(message);
-    mongoose.connection.close()
-  })
+async function connectToDatabase() {
+  try {
+    await mongoose.connect(url);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
 }
+
+async function saveContact(name, number) {
+
+  const contact = new Contact({ name, telephone: number });
+
+  try {
+    await contact.save();
+    console.log(`Added ${name} with number ${number} to phonebook.`);
+  } catch (error) {
+    console.error("Error saving contact:", error);
+  }
+}
+
+async function listContacts() {
+  
+  try {
+    const contacts = await Contact.find({});
+    const message = contacts.reduce((msg, contact) => {
+      return `${msg}\n${contact.name} ${contact.telephone}`;
+    }, "Phonebook:");
+    console.log(message);
+  } catch (error) {
+    console.error("Error fetching contacts:", error);
+  }
+}
+
+(async () => {
+  await connectToDatabase();
+
+  if (contactName && contactNumber) {
+    await saveContact(contactName, contactNumber);
+  } else {
+    await listContacts();
+  }
+
+  mongoose.connection.close();
+})();
