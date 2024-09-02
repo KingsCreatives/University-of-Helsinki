992761a67deafa09889108a3116fb2bc296ef4e5
kwame cody <kwamecody@gmail.com>
2024-09-02 17:58:10 +0000

    Initialize Blog List application with Express and MongoDB(Exercise 4.1)
    - Set up the project as an npm package.
    - Added initial Express server with routes to handle blog entries.
    - Configured nodemon for automatic server restarts during development.
    - Verified blog addition and retrieval via Postman.

---
+++ part4/blog/app.js
@@ -0,0 +1,31 @@
+const {MONGODB_URI} = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogRouter = require("./controllers/blog");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDB:", error.message);
+  });
+
+app.use(cors());
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ part4/blog/controllers/blog.js
@@ -0,0 +1,22 @@
+const blogRouter = require("express").Router();
+const Blog = require("../models/blog");
+
+blogRouter.get("/", (req, res) => {
+  Blog.find({})
+    .then((blogs) => {
+      res.json(blogs);
+    })
+    .catch((error) => next(error));
+});
+
+blogRouter.post("/", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((error) => next(error));
+});
+
+module.exports = blogRouter
