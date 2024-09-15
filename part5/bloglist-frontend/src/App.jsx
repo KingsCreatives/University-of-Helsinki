9f56eebb0fed83f7f3db48373f83e7c8c9892558
kwame cody <kwamecody@gmail.com>
2024-09-15 04:11:28 +0000

    5.1: Implemented login functionality and conditional rendering for Blog List Frontend

---
+++ part5/bloglist-frontend/src/App.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
+
+const App = () => {
+  const [blogs, setBlogs] = useState([]);
+  // const [newBlog, setNewBlog] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({
+        username,
+        password,
+      });
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.log("There was an error logging you in");
+    }
+  };
+
+  return (
+    <div>
+      <h2>{user ? "blogs" : "log in to application"}</h2>
+      {!user ? (
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      ) : (
+        <div>
+          <p>{user.username} logged in</p>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
